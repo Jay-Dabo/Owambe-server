1bf0b6d53536a0c6ffb398ee972214e5c6becc45
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-10-09 15:14:34 +0000

    MongoDb Connection created

--- routes/api.js
+++ routes/api.js
@@ -2,5 +2,14 @@ const express = require('express'); // Require Express Server
 const jwt = require('jsonwebtoken'); // Use JSON Web Token for Authentications
 const router = express.Router(); // Require Routing for API endpints
 
+// Connect to Database in MongoDb
+const db = "mongodb+srv://owambe-admin:kxygMXGuyPEV59x1@owambe.u03su.mongodb.net/Owambe?retryWrites=true&w=majority"
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }, err => {
+    if (err) {
+        console.log('Sorry!! There has been an error: ' + err)
+    } else {
+        console.log('Successfully connected to database!!')
+    }
+});
 
 module.exports = router
