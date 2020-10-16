f76030b43f2b3d61f851ced62c01a33ad2c8e90b
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-10-16 16:30:08 +0000

    Configuration of GStore-Node and Firestore

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,30 @@
+const config = require('../config.js'); // Configuration File
 const express = require('express'); // Require Express Server
 const jwt = require('jsonwebtoken'); // Use JSON Web Token for Authentications
 const router = express.Router(); // Require Routing for API endpints
 
 
-// // Require Mongooge for connection to Database
-// const mongoose = require('mongoose');
+// Require Google Cloud APIs for connection to Database
+const { Firestore } = require('@google-cloud/firestore');
+const { Gstore, instances } = require('gstore-node');
 
-// // Connect to Database in MongoDb
-// const db = "mongodb+srv://owambe-admin:kxygMXGuyPEV59x1@owambe.u03su.mongodb.net/Owambe?retryWrites=true&w=majority"
-// mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }, err => {
-//     if (err) {
-//         console.log('Sorry!! There has been an error: ' + err)
-//     } else {
-//         console.log('Successfully connected to database!!')
-//     }
-// });
+const gstore = new Gstore();
+const fstore = new Firestore({
+    projectId: config.projectId,
+    keyFilename: config.keyFilename
+});
+
+// Conect Google Cloud Firestore to the Database Instance
+gstore.connect(fstore);
+
+// Save the gstore instance
+instances.set('default', gstore);
+
+
+
+
+
+
+
 
 module.exports = router
