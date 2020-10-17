073f2ef2d9ddb8c42dfc7cb1960f20658879971c
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-10-17 21:19:55 +0000

    Restructuring of the User Model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
-const firearch = require('firearch');
-const Schema = firearch.Schema;
+const mongoose = require('mongoose'); // Require Mongooge for connection to Database
+const Schema = mongoose.Schema // Require Schema Instace of Mongoose
+const bcrypt = require('bcryptjs'); // Require BCrypt for Password encryption
 
+// Create Schema for Users in MongoDb
 const userSchema = new Schema({
-  	first_name: { type: String, required: true },
+    first_name: { type: String, required: true },
     other_names: { type: String },
     last_name: { type: String, required: true },
     gender: { type: String, required: true },
@@ -10,6 +12,8 @@ const userSchema = new Schema({
     phone_number: { type: Number, required: true },
     birth_date: { type: Date, format: 'YYYY-mm-dd', required: true },
     address: { type: String, required: true },
+    post_office_address: { type: String },
+    is_active: { type: Boolean, default: false },
     password: { type: String, min: [8, 'Too short, min 4 characters are required'], required: true },
     password_confirmation: { type: String, min: [8, 'Too short, min 4 characters are required'], required: true },
 },
@@ -17,5 +21,27 @@ const userSchema = new Schema({
     timestamps: true 
 });
 
-// Export Model with the User schema into User Collection on Firestore
-module.exports = firearch.model('user', userSchema, 'Users');
+userSchema.pre('save', function(next) {
+    const user = this
+    bcrypt.genSalt(15, function(error, salt) {
+        if (error) {
+            return res.status(422).send('There is an error while generatiing salt hash')
+        }
+        bcrypt.hash(user.password, salt, function(error, hash) {
+            if (error) {
+                return res.status(422).send('There is an error with password hashing')
+            }
+            // Replace Password field values with hashed password
+            user.password = hash
+            user.password_confirmation = hash
+            next()
+        });
+    });
+});
+
+userSchema.methods.hasSamePassword = function(password) {
+    return bcrypt.compareSync(password, this.password)
+};
+
+// Export Model with the User schema into User Collection on MongoDb
+module.exports = mongoose.model('user', userSchema, 'Users');
