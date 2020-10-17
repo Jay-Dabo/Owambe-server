64da1c385ad794a9c0027de69998d76ca7abf4e0
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-10-17 21:13:24 +0000

    Reintegration of Mongoose for Database Dependency

--- routes/api.js
+++ routes/api.js
@@ -2,32 +2,19 @@ const config = require('../config.js'); // Configuration File
 const express = require('express'); // Require Express Server
 const jwt = require('jsonwebtoken'); // Use JSON Web Token for Authentications
 const router = express.Router(); // Require Routing for API endpints
-const firearch  =  require('firearch'); // Require Firearch for Firestore object modeling
 
-// Require Google Cloud Firestore API for connection to Database
-const { Firestore } = require('@google-cloud/firestore');
-const fstore = new Firestore({
-    projectId: config.projectId,
-    keyFilename: config.keyFilename
-});
+// Require Mongooge for connection to Database
+const mongoose = require('mongoose');
 
-// Conect Google Cloud Firestore to the Database Instance
-firearch.connect(fstore, error => {
-	if (error) {
-        console.log('Sorry!! There has been an error: ' + error)
+// Connect to Database in MongoDb
+const db = "mongodb+srv://openEdu-Admin:Sb0ekTExb5xnI3ef@openeducation.3hyrm.mongodb.net/openEducation_System?retryWrites=true&w=majority"
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }, err => {
+    if (err) {
+        console.log('Sorry!! There has been an error: ' + err)
     } else {
-        console.log('Successfully connected to database!! Keep building!')
+        console.log('Successfully connected to database!! Keep building!!')
     }
 });
 
-
-// Controller Imports
-const User = require('../controllers/userController');
-
-
-
-
-// Routings for Users
-router.post('/user/register', User.register);
 
 module.exports = router
