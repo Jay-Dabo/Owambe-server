98091aa50514622b459589ee6b4578be5d4a59ac
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-10-17 19:06:19 +0000

    Adding Firearch Dependency for Firestore Schema Creation and Schematype Validation

--- routes/api.js
+++ routes/api.js
@@ -2,23 +2,23 @@ const config = require('../config.js'); // Configuration File
 const express = require('express'); // Require Express Server
 const jwt = require('jsonwebtoken'); // Use JSON Web Token for Authentications
 const router = express.Router(); // Require Routing for API endpints
+const firearch  =  require('firearch'); // Require Firearch for Firestore object modeling
 
-
-// Require Google Cloud APIs for connection to Database
+// Require Google Cloud Firestore API for connection to Database
 const { Firestore } = require('@google-cloud/firestore');
-const { Gstore, instances } = require('gstore-node');
-
-const gstore = new Gstore();
 const fstore = new Firestore({
     projectId: config.projectId,
     keyFilename: config.keyFilename
 });
 
 // Conect Google Cloud Firestore to the Database Instance
-gstore.connect(fstore);
-
-// Save the gstore instance
-instances.set('default', gstore);
+firearch.connect(fstore, error => {
+	if (error) {
+        console.log('Sorry!! There has been an error: ' + error)
+    } else {
+        console.log('Successfully connected to database!! Keep building!')
+    }
+}); 
 
 
 
