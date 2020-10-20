2294bc49e6ab8819af8f60c1191d0168da047e18
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-10-20 13:47:11 +0000

    Organization Model created

---
+++ models/organization.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose'); // Require Mongooge for connection to Database
+const Schema = mongoose.Schema // Require Schema Instace of Mongoose
+const bcrypt = require('bcryptjs'); // Require BCrypt for Password encryption
+
+// Create Schema for Organizations in MongoDb
+const organizationSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, lowercase: true, match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/], unique: true, required: true },
+    phone_number: { type: Number, required: true },
+    address: { type: String, required: true },
+    post_office_address: { type: String },
+    account_balance: { type: Number, default: 200000 },
+    is_active: { type: Boolean, default: false },
+    password: { type: String, min: [8, 'Too short, min 4 characters are required'], required: true },
+    password_confirmation: { type: String, min: [8, 'Too short, min 4 characters are required'], required: true },
+},
+{ 
+    timestamps: true 
+});
+
+organizationSchema.pre('save', function(next) {
+    const organization = this
+    bcrypt.genSalt(15, function(error, salt) {
+        if (error) {
+            return res.status(422).send('There is an error while generatiing salt hash')
+        }
+        bcrypt.hash(organization.password, salt, function(error, hash) {
+            if (error) {
+                return res.status(422).send('There is an error with password hashing')
+            }
+            // Replace Password field values with hashed password
+            organization.password = hash
+            organization.password_confirmation = hash
+            next()
+        });
+    });
+});
+
+organizationSchema.methods.hasSamePassword = function(password) {
+    return bcrypt.compareSync(password, this.password)
+};
+
+// Export Model with the Organization schema into Organization Collection on MongoDb
+module.exports = mongoose.model('organization', organizationSchema, 'Organizations');
