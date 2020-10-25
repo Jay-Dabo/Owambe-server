e9562ac0a632216e58355672a10134bb5d79a01f
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-10-25 08:15:48 +0000

    Added Fundraiser query dependency to Specific Category Query

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,6 @@
 // Model Imports
 const Category = require('../models/category')
+const Fundraiser = require('../models/fundraiser')
 
 
 
@@ -14,13 +15,17 @@ exports.all = function(req, res) {
 }
 
 exports.one = function(req, res) {
-    Category.findById(req.params._id)
-        .populate('donations')
-        .exec(function(error, category) {
+    Category.findById(req.params._id, function(error, category) {
             if (error) {
                 return res.status(404).send('Sorry!! The queried category could not be found or does not exist in our database')
             } else {
-                return res.status(200).json(category)
+                Fundraiser.findOne({ categories: req.params._id }, function(error, fundraisers) {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        return res.status(200).json(fundraisers)
+                    }
+                });
             }
         }
     );
