b825edbd5119e3e99bc22c51033ebd32f49c2028
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-10-25 14:13:11 +0000

    Category Model update

--- models/category.js
+++ models/category.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema // Require Schema Instace of Mongoose
 // Create Schema for Category in MongoDb
 const categorySchema = new Schema({
     _id: { type: String },
-    name: { type: String, unique: true, required: true }
+    name: { type: String, unique: true, required: true },
+    description: { type: String, required: true }
 });
 
 module.exports = mongoose.model('category', categorySchema, 'Categories')
